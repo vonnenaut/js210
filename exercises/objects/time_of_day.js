18fdc241c9cc3ec74d862f697c35e9c2ae1c1cf7
vonnenaut <danielashcom@gmail.com>
2020-11-20 23:20:51 +0700

    automated sync commit for js210

--- exercises/objects/time_of_day.js
+++ exercises/objects/time_of_day.js
@@ -1,17 +1,39 @@
-const MINUTES_PER_HOUR = 60;
-const HOURS_PER_DAY = 24;
-const MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR;
+// const MINUTES_PER_HOUR = 60;
+// const HOURS_PER_DAY = 24;
+// const MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR;
+
+// function timeOfDay(deltaMinutes) {
+//   deltaMinutes = deltaMinutes % MINUTES_PER_DAY;
+//   if (deltaMinutes < 0) {
+//     deltaMinutes = MINUTES_PER_DAY + deltaMinutes;
+//   }
+
+//   let hours = Math.floor(deltaMinutes / MINUTES_PER_HOUR);
+//   let minutes = deltaMinutes % MINUTES_PER_HOUR;
+
+//   return `${padWithZeroes(hours, 2)}:${padWithZeroes(minutes, 2)}`;
+// }
+
+// function padWithZeroes(number, length) {
+//   let numberString = String(number);
+
+//   while (numberString.length < length) {
+//     numberString = `0${numberString}`;
+//   }
+
+//   return numberString;
+// }
+
+// reimplementation of the above using JS' Date object
+const MILLISECONDS_PER_MINUTE = 60000;
 
 function timeOfDay(deltaMinutes) {
-  deltaMinutes = deltaMinutes % MINUTES_PER_DAY;
-  if (deltaMinutes < 0) {
-    deltaMinutes = MINUTES_PER_DAY + deltaMinutes;
-  }
+  const midnight = new Date('1/1/2000 00:00');
+  const afterMidnight = new Date(midnight.getTime() + deltaMinutes * MILLISECONDS_PER_MINUTE);
+  const hours = padWithZeroes(afterMidnight.getHours(), 2);
+  const minutes = padWithZeroes(afterMidnight.getMinutes(), 2);
 
-  let hours = Math.floor(deltaMinutes / MINUTES_PER_HOUR);
-  let minutes = deltaMinutes % MINUTES_PER_HOUR;
-
-  return `${padWithZeroes(hours, 2)}:${padWithZeroes(minutes, 2)}`;
+  return `${hours}:${minutes}`;
 }
 
 function padWithZeroes(number, length) {
@@ -24,16 +46,11 @@ function padWithZeroes(number, length) {
   return numberString;
 }
 
-// reimplementation of the above using JS' Date object
-function timeOfDay() {
-  
-}
-
 // Test
-timeOfDay(0);          // "00:00"
-timeOfDay(-3);         // "23:57"
-timeOfDay(35);         // "00:35"
-timeOfDay(-1437);      // "00:03"
-timeOfDay(3000);       // "02:00"
-timeOfDay(800);        // "13:20"
-timeOfDay(-4231);      // "01:29"
+console.log(timeOfDay(0) === "00:00");  // "00:00"
+console.log(timeOfDay(-3) === "23:57"); // "23:57"
+console.log(timeOfDay(35) === "00:35"); // "00:35"
+console.log(timeOfDay(-1437) === "00:03"); // "00:03"
+console.log(timeOfDay(3000) === "02:00"); // "02:00"
+console.log(timeOfDay(800) === "13:20"); // "13:20"
+console.log(timeOfDay(-4231) === "01:29"); // "01:29"
